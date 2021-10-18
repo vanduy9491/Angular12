cb6a5b68b7e2881f1d739323474b9bea1fc23742
tran van duy <vanduy9491@gmail.com>
2021-10-18 09:07:00 +0700

    Databinding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public name = 'Duy Trần';
+  public age = 19;
   constructor() { }
 
   ngOnInit(): void {
